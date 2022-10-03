f6996292aedd1bf9157403ac4df55bd34ce4b3fb
Dnyaneshwari <dnyaneshwariumap994@gmail.com>
2022-10-04 01:06:13 +0530

    Person details

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,8 @@
 package AddressSystem;
-
+import java.util.Scanner;
 public class AddressBook {
     public static void main(String[]args){
         System.out.println("Welcome to address book system");
+
     }
 }
